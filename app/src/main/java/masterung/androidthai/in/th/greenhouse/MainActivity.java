662686a6abadff5b26b03710202182d4da07a9e0
masterUNG <phrombutr@gmail.com>
2018-11-27 09:39:18 +0700

    Show View Success

--- app/src/main/java/masterung/androidthai/in/th/greenhouse/MainActivity.java
+++ app/src/main/java/masterung/androidthai/in/th/greenhouse/MainActivity.java
@@ -1,11 +1,17 @@
 package masterung.androidthai.in.th.greenhouse;
 
 import android.graphics.Color;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,11 +25,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 //        Setup Color
-        TextView textView = findViewById(R.id.txtNumber);
+        final TextView textView = findViewById(R.id.txtNumber);
         textView.setTextColor(Color.BLUE);
 
 //        Connected Database Firebase
         FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+        DatabaseReference databaseReference = firebaseDatabase.getReference().child("FromNode");
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                NumberModel numberModel = dataSnapshot.getValue(NumberModel.class);
+                int numberInt = numberModel.getNumberAnInt();
+                Log.d("27novV1", "numberInt ==> " + numberInt);
+
+                textView.setText(Integer.toString(numberInt));
+
+            }   // onDataChange
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
 
 
 
